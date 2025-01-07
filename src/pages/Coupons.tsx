2bc263c20b6a228096d06f479d28254857498294
12209123 <k12209123@students.jku.at>
2025-01-07 18:21:40 +0100

    added basic pages

--- src/pages/Coupons.tsx
+++ src/pages/Coupons.tsx
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+
+const Coupons: React.FC = () => {
+  return (
+    <div>
+      <h1>Coupon Page</h1>
+      <p>Welcome to your Coupon page.</p>
+    </div>
+  );
+};
+
+export default Coupons;
--- src/pages/CreateTask.tsx
+++ src/pages/CreateTask.tsx
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+
+const CreateTask: React.FC = () => {
+  return (
+    <div>
+      <h1>Create Task Page</h1>
+      <p>Welcome to your account page.</p>
+    </div>
+  );
+};
+
+export default CreateTask;
