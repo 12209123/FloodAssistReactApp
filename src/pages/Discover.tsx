33dd2a69ddbeafc83e7322f48307ffae33128dde
Lukas Werner <lukas.werner001@gmail.com>
2025-01-08 20:30:47 +0100

    fixed icon in prod, added different icons when registered/owned

--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -9,10 +9,17 @@ import {
 import "leaflet/dist/leaflet.css";
 import { Link } from "react-router-dom";
 import { emergencies } from "../data/emergencies";
+import L from "leaflet";
+import iconUrl from "../../public/marker-icon.svg";
+import iconUrl2 from "../../public/marker-icon2.svg";
+import {
+  getCurrentRegisteredEmergency,
+  getCurrentUserId,
+} from "../globalRegistrationStore";
 
 interface MapProps {
   onPositionSelect: (lat: number, lng: number) => void;
-  showEmergencies?: boolean; // new prop
+  showEmergencies?: boolean;
 }
 
 const SelectPositionMap: React.FC<MapProps> = ({ onPositionSelect }) => {
@@ -26,11 +33,27 @@ const SelectPositionMap: React.FC<MapProps> = ({ onPositionSelect }) => {
   return null;
 };
 
+// Normal (default) marker
+const MarkerIcon = L.icon({
+  iconUrl: iconUrl,
+  iconSize: [32, 32], // Adjust size as needed
+  iconAnchor: [16, 32], // The "tip" of the marker
+  popupAnchor: [0, -32],
+});
+
+// Registered/Owned marker
+const MarkerIconRegOwn = L.icon({
+  iconUrl: iconUrl2,
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
+
 const MapView: React.FC<MapProps> = ({
   onPositionSelect,
-  showEmergencies = true, // default to `true` if not provided
+  showEmergencies = true,
 }) => {
-  const mapCenter = [48.30694, 14.28583];
+  const mapCenter: [number, number] = [48.30694, 14.28583];
   const zoomLevel = 12;
 
   return (
@@ -54,21 +77,34 @@ const MapView: React.FC<MapProps> = ({
 
         {/* Conditionally render emergencies */}
         {showEmergencies &&
-          emergencies.map((emergency, idx) => (
-            <Marker key={idx} position={emergency.position}>
-              <Popup>
-                <strong>{emergency.title}</strong>
-                <br />
-                {emergency.description}
-                <br />
-                <strong>Priority:</strong> {emergency.priority}
-                <br />
-                <Link to={`/emergency/${emergency.id}`}>
-                  <button style={{ marginTop: "8px" }}>More Details</button>
-                </Link>
-              </Popup>
-            </Marker>
-          ))}
+          emergencies.map((emergency) => {
+            const isOwner = emergency.ownerId === getCurrentUserId();
+            const isRegistered =
+              getCurrentRegisteredEmergency() === emergency.id;
+
+            // If user is owner or registered, use the second icon
+            const icon =
+              isOwner || isRegistered ? MarkerIconRegOwn : MarkerIcon;
+
+            return (
+              <Marker
+                key={emergency.id}
+                position={emergency.position}
+                icon={icon}>
+                <Popup>
+                  <strong>{emergency.title}</strong>
+                  <br />
+                  {emergency.description}
+                  <br />
+                  <strong>Priority:</strong> {emergency.priority}
+                  <br />
+                  <Link to={`/emergency/${emergency.id}`}>
+                    <button style={{ marginTop: "8px" }}>More Details</button>
+                  </Link>
+                </Popup>
+              </Marker>
+            );
+          })}
       </MapContainer>
     </div>
   );
